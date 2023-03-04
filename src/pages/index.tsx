017796fe04a08c384ec0344a149b5d33342a28f5
andreasrobert <andreasrobb23@gmail.com>
2023-03-04 16:57:45 +0700

    Update editor type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
-import Head from "next/head";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 // Import the Slate editor factory.
-import { createEditor, BaseEditor, Descendant } from "slate";
+import {
+  createEditor,
+  BaseEditor,
+  Descendant,
+  Transforms,
+  Editor,
+  Element as SlateElement,
+} from "slate";
 
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact, ReactEditor } from "slate-react";
 
-type CustomElement = { type: "paragraph"; children: CustomText[] };
+type CustomElement = { type: "paragraph" | "code"; children: CustomText[] };
 type CustomText = { text: string };
 
 declare module "slate" {
@@ -28,13 +34,57 @@ const initialValue: Descendant[] = [
 export default function Home() {
   const [editor] = useState(() => withReact(createEditor()));
 
+  const renderElement = useCallback((props: any) => {
+    console.log(props);
+    switch (props.element.type) {
+      case "code":
+        return <CodeElement {...props} />;
+      default:
+        return <DefaultElement {...props} />;
+    }
+  }, []);
+
   return (
     <main>
       <div className="container">
         <Slate editor={editor} value={initialValue}>
-          <Editable />
+          <Editable
+            renderElement={renderElement}
+            onKeyDown={(event) => {
+              if (event.key === "`" && event.ctrlKey) {
+                event.preventDefault();
+                // Determine whether any of the currently selected blocks are code blocks.
+                const [match] = Editor.nodes(editor, {
+                  match: (n) => SlateElement.isElement(n) && n.type === "code",
+                });
+                // Toggle the block type depending on whether there's already a match.
+                Transforms.setNodes(
+                  editor,
+                  { type: match ? "paragraph" : "code" },
+                  { match: (n) => SlateElement.isElement(n) && Editor.isBlock(editor, n) }
+                );
+              }
+            }}
+          />
         </Slate>
       </div>
     </main>
   );
 }
+
+type CodeElementProps = {
+  attributes: React.HTMLAttributes<HTMLPreElement | HTMLParagraphElement>;
+  children: React.ReactNode;
+};
+
+const CodeElement = (props: CodeElementProps) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  );
+};
+
+const DefaultElement = (props: CodeElementProps) => {
+  return <p {...props.attributes}>{props.children}</p>;
+};
